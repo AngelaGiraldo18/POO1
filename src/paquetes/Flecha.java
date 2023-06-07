4adaa992cf48e5ae863c25801f71fad0c4f8987f
AngelaGiraldo18 <at593987@gmail.com>
2023-06-06 20:49:42 -0600

    first commit

---
+++ src/paquetes/Flecha.java
@@ -0,0 +1,34 @@
+package paquetes;
+
+public class Flecha {
+    int longitud;
+    String color;
+   
+    public Flecha() {
+        longitud = 18;
+    	color="negro";
+    }
+    
+    public Flecha(int longitud,String color) {
+    	this.longitud=longitud;
+    	this.color=color;
+    	
+    }
+    
+    public void imprimirEspacio() {
+    	System.out.println();
+    }
+    public void construirFlecha() {
+    	for (int i = 0; i < longitud; i++) {
+			imprime("-");
+		}
+    	imprime(">");
+    }
+    private void imprime(String simbolo) {
+    	if (color.equals("negro")) {
+			System.out.println(simbolo);
+		}else {
+			System.out.println(simbolo);
+		}
+    }
+}
